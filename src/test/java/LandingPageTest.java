06eb16a739f26b15d401ccba1d1c2596e5cb9417
Michael Kamau <tokamau@gmail.com>
2018-02-27 12:49:52 +0300

    Add test view Promo messages

--- src/test/java/LandingPageTest.java
+++ src/test/java/LandingPageTest.java
@@ -1,12 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LandingPage;
 
-public class LandingPageTest  extends  BaseTest{
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    private final LandingPage landingPage = new LandingPage();
-
+public class LandingPageTest extends BaseTest {
     @Test
-    public void viewSignUpButtonTest(){
-        System.out.println("testing ");
+    public void viewPromoLabelsTest() {
+        List<WebElement> promoLabels = LandingPage.getPromoLabels(driver);
+        List<String> promoLabelsStrings = promoLabels
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        String[] expectedLabels = {
+                "Follow your interests.",
+                "Hear what people are talking about.",
+                "Join the conversation."
+        };
+
+        Assert.assertEquals(promoLabelsStrings, Arrays.asList(expectedLabels));
+
     }
 }
