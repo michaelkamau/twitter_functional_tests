33f91edcf927fb5e26aa7cd92d39d4ee02ffa5d0
Michael Kamau <tokamau@gmail.com>
2018-03-05 11:24:21 +0300

    Add common constants

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,16 +1,21 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import pages.BasePage;
+
+import java.util.logging.Logger;
 
 public class BaseTest {
 
     final String chromeDriverPath = System.getenv("SELENIUM_DRIVERS_DIR") + "/chromedriver";
+    final static Logger logger = Logger.getLogger("twitter_functional_tests");
+    final String validEmail = System.getenv("validEmail");
+    final String validHandle = System.getenv("validHandle");
+    final String validPassword = System.getenv("validPassword");
+    final String invalidPassword = "not_a_password";
+    final String invalidEmail = "not_an_email";
+    final String invalidHandle = "not_a_handle";
     WebDriver driver;
-
-
 
     @BeforeClass
     public void setUp() throws Exception {
@@ -20,7 +25,7 @@ public class BaseTest {
     }
 
     @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
+    public void tearDown() {
         if (driver != null)
             driver.quit();
     }
