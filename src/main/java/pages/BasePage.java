94d981d3740f9247747c15e5d8c802138596f18f
Michael Kamau <tokamau@gmail.com>
2018-03-06 12:03:05 +0300

    Add notification url getter

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,8 +2,13 @@ package pages;
 
 public class BasePage {
     private final static String BASE_URL = "https://twitter.com/";
+    private final static String notificationsUrl = BASE_URL + "i/notifications";
 
-    public static String getBaseUrl() {
+    public final static String getBaseUrl() {
         return BASE_URL;
+    }
+
+    public final static String getNotificationsUrl() {
+        return notificationsUrl;
     }
 }
