6d979c632b96bc1d49931ac2bbaaebbb61b6c6b3
Michael Kamau <tokamau@gmail.com>
2018-03-14 14:33:20 +0300

    Refactor to use Selenium Page Object model

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -1,52 +1,43 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import java.util.List;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 public class LandingPage extends BasePage{
     private final WebDriver driver;
 
+    @FindBy(how = How.CSS, using = ".StaticLoggedOutHomePage-login > form:nth-child(1) > div:nth-child(1) > input:nth-child(1)")
+    private WebElement passwordInputField;
+
+    @FindBy(how = How.CSS, using = ".StaticLoggedOutHomePage-login > form:nth-child(1) > div:nth-child(2) > input:nth-child(1)")
+    private WebElement identifierInputField;
+
+    @FindBy(how = How.CSS, using = "input.EdgeButton:nth-child(3)")
+    private WebElement logInButton;
+
+    @FindBy(how = How.CSS, using = "a.forgot:nth-child(1)")
+    private WebElement forgotPasswordLink;
+
+    @FindBy(how = How.CSS, using = "div.StaticLoggedOutHomePage-field:nth-child(6) > input:nth-child(1)")
+    private WebElement joiningIdentifierInputField;
+
+    @FindBy(how = How.CSS, using = "div.StaticLoggedOutHomePage-field:nth-child(7) > input:nth-child(1)")
+    private WebElement joiningPasswordInputField;
+
+    @FindBy(how = How.CSS, using = "button.EdgeButton--medium:nth-child(1)")
+    private WebElement getStartedButton;
+
+    @FindBy(how = How.CSS, using = ".StaticLoggedOutHomePage-signupTitle")
+    private WebElement signUpTitleLabel;
+
     public LandingPage(WebDriver driver) {
         this.driver = driver;
-        this.driver.get(getBaseUrl());
     }
 
-    public static WebElement getPasswordInputField(WebDriver driver){
-        return null;
-    }
-
-    public static WebElement getUsernameInputField(WebDriver driver){
-        return null;
-    }
-
-    public static WebElement getForgotPasswordLink(WebDriver driver){
-        return null;
-    }
-
-    public static WebElement getLogInButton(WebDriver driver){
-        return null;
-    }
-
-    public static WebElement getJoiningPasswordField(WebDriver driver){
-        return null;
-    }
-
-    public static WebElement getJoiningPhoneEmailField(WebDriver driver){
-        return null;
-    }
-
-
-    public static WebElement getGettingStartedBtn(WebDriver driver){
-        return null;
-    }
-
-    public List<WebElement> getPromoLabels(){
-        By promoIdent = By.cssSelector("div.StaticLoggedOutHomePage-communicationContent .StaticLoggedOutHomePage-communicationItem");
-        List<WebElement> promos = this.driver.findElements(promoIdent);
-        return promos;
+    public final String getLoggedOutUrl() {
+        return driver.getCurrentUrl();
     }
 
 }
